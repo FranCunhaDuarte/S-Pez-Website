262f49ad83226dfb7ed4ec4d2e10a026e303ac65
Franco Cunha Duarte <francunhaduarte@gmail.com>
2023-08-15 20:15:33 -0300

    Almost ending the index

--- javascript/main.js
+++ javascript/main.js
@@ -8,16 +8,3 @@ menuBtn.addEventListener('click',()=>{
 const header=document.querySelector('.header-index');
 const navBarMobile=document.querySelector('.navBar-mobile');
 
-
-function myFunction(x) {
-    if (x.matches) {
-        header.removeChild(navBarMobile);
-        document.body.style.backgroundColor = "lightblue";
-    } else {
-        document.body.style.backgroundColor = "pink";
-    }
-  }
-  
-  var x = window.matchMedia("(min-width: 1024px)");
-  myFunction(x);
-  x.addListener(myFunction);
